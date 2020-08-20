09dd47b33ffde60b15bd2ab73c6abe9705f86f57
Adian <QRXwzx@Outlook.com>
2020-08-20 09:49:41 +0800

    Modify TransformationDriver.java to add configuration for test code splitting.

--- src/main/java/nju/pa/driver/TransformationDriver.java
+++ src/main/java/nju/pa/driver/TransformationDriver.java
@@ -1,15 +1,14 @@
 package nju.pa.driver;
 
 import nju.pa.exception.InvalidCmdOptionException;
+import nju.pa.transform.CodeTransformer;
+import nju.pa.transform.TestCodeSplitter;
 import nju.pa.transform.TestCodeTransformer;
 import nju.pa.util.IOUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Main class, Driver will drive the whole transformation process.
@@ -25,14 +24,11 @@ public class TransformationDriver {
     // option - param
     private static final Map<String, String> paramsMap = new HashMap<>();
 
-//    private static final String CHARSET = "ISO-8859-1";
-//    private static final String CHARSET = "KSC5601";
-//    private static final String CHARSET = "UTF-8";
-
     static {
         paramsMap.put("-d", EMPTY_PARAM); // -d: A directory which contains .java files want transforming.
         paramsMap.put("-f", EMPTY_PARAM); // -f: A single file which wants transforming.
-        paramsMap.put("-c", EMPTY_PARAM); // -c: In generated java file: comment on or off(true or false).
+        paramsMap.put("-c", EMPTY_PARAM); // -c: In generated java file: comment on or off (true or false).
+        paramsMap.put("-s", EMPTY_PARAM); // -s: Split test code source into the form the each class only contains one test annotated method.
     }
 
     public static void main(String[] args) {
@@ -44,24 +40,42 @@ public class TransformationDriver {
             // TODO - Change this part if param classes is enlarged.
             paramsMap.put(option, args[i + 1]);
         }
-
         System.out.println("[paramsMap]" + paramsMap);
 
+        // Process options.
         boolean commentOn = true;
         if(!emptyOption("-c"))
             commentOn = Boolean.parseBoolean(paramsMap.get("-c"));
-
-        if(!emptyOption("-d")) { // -d is not empty
+        // Collect all test java files that need to be transformed.
+        List<File> allTestJavaFiles = new ArrayList<>();
+        if(!emptyOption("-d")) {
             String dirPath = paramsMap.get("-d");
             List<File> testJavaFiles = IOUtil.getAllFilesBySuffix(dirPath, IOUtil.JAVA_SUFFIX);
-            // Transform all java files.
+            allTestJavaFiles.addAll(testJavaFiles);
+        }
+        if(!emptyOption("-f")) {
+            String javaPath = paramsMap.get("-f");
+            allTestJavaFiles.add(new File(javaPath));
+        }
+        // Decide which code transformer to use.
+        boolean useSplitter = false;
+        if(!emptyOption("-s"))
+            useSplitter = Boolean.parseBoolean(paramsMap.get("-s"));
+        if(useSplitter) {
+            // Split all test java files.
+            TestCodeSplitter splitter = new TestCodeSplitter();
+            for (File testJavaFile : allTestJavaFiles) {
+                splitter.setJavaFile(testJavaFile);
+                splitter.output(commentOn);
+            }
+        } else {
+            // Transform all test java files.
             TestCodeTransformer transformer = new TestCodeTransformer();
-            for (File testJavaFile : testJavaFiles) {
+            for (File testJavaFile : allTestJavaFiles) {
                 transformer.setJavaFile(testJavaFile);
                 System.out.println(String.format("[LOG] Now process [%s]", testJavaFile.getAbsolutePath()));
                 try {
                     IOUtil.writeContentIntoFile(testJavaFile, transformer.transformToSrc(commentOn));
-//                    IOUtil.writeContentIntoFile(testJavaFile, transformer.transformToSrc(commentOn), CHARSET);
                 } catch (IOException e) {
                     e.printStackTrace();
                     System.out.println(String.format("[LOG] Write content to [%s] failed.", testJavaFile.getAbsolutePath()));
@@ -69,21 +83,6 @@ public class TransformationDriver {
                 }
             }
         }
-
-        if(!emptyOption("-f")) { // -d is not empty
-            String javaPath = paramsMap.get("-f");
-            TestCodeTransformer transformer = new TestCodeTransformer(javaPath);
-            System.out.println(String.format("[LOG] Now process %s", javaPath));
-            try {
-                IOUtil.writeContentIntoFile(javaPath, transformer.transformToSrc(commentOn));
-//                IOUtil.writeContentIntoFile(javaPath, transformer.transformToSrc(commentOn), CHARSET);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println(String.format("[LOG] Write content to [%s] failed.", javaPath));
-                System.exit(-1);
-            }
-        }
-
     }
 
     private static boolean emptyOption(String option) {
@@ -104,7 +103,10 @@ public class TransformationDriver {
                 .append("\t -f : A single file which wants transforming.")
                 .append(NEW_LINE)
                 .append("\t -c: In generated java file: comment on or off(true or false). " +
-                        "As default, generated java file will contains comment to show some lines's original code.");
+                        "As default, generated java file will contains comment to show some lines's original code.")
+                .append(NEW_LINE)
+                .append("\t -s: Split test code source into the form the each class only " +
+                        "contains one test annotated method. (true or false)");
         return msgBuilder.toString();
     }
 
